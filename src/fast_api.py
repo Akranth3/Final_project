c044c0c9eaf15d1f2f6e60a30b2365fdaab01532
Akranth <akranthreddy@gmail.com>
2024-05-17 11:25:37 +0530

    prometheous is working

--- src/fast_api.py
+++ src/fast_api.py
@@ -3,6 +3,7 @@ import io
 import PIL
 import PIL.Image as Image
 import PIL.ImageOps  
+import psutil
 sys.path.append("..")
 
 from fastapi import FastAPI, File, UploadFile, Response
@@ -28,13 +29,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 # Define counters for API usage
-api_usage_counter = Counter("api_usage", "API usage counter", ["client_ip"])
+# api_usage_counter = Counter("api_usage", "API usage counter", ["client_ip"])
 
-Instrumentator().instrument(app).expose(app)
-
+Instrumentator().instrument(app=app).expose(app)
+num_requests = Counter('num_requests', 'Number of requests received', ['method', 'endpoint', 'ip_address'])
+memory_usage = Gauge('memory_usage_bytes', 'Memory usage in bytes')
+cpu_usage = Gauge('cpu_usage_percent', 'CPU usage percentage')
+network_io_sent = Counter('network_io_sent_bytes_total', 'Total number of bytes sent via network')
+network_io_received = Counter('network_io_received_bytes_total', 'Total number of bytes received via network')
 # Define gauges for API processing time
-api_time_gauge = Gauge("api_time", "API processing time", ["client_ip"])
-api_time_per_char_gauge = Gauge("api_time_per_char", "API processing time per character", ["client_ip"])
+# api_time_gauge = Gauge("api_time", "API processing time", ["client_ip"])
+# api_time_per_char_gauge = Gauge("api_time_per_char", "API processing time per character", ["client_ip"])
 
 def load_model():
     model = build_densenet_based_model()
@@ -56,20 +61,23 @@ def predict_label(image, model):
     
     return prediction
     
-@app.get("/metrics/")
-def get_metrics():
-    return Response(prometheus_client.generate_latest(), media_type="text/plain")
+# @app.get("/metrics/")
+# def get_metrics():
+#     return Response(prometheus_client.generate_latest(), media_type="text/plain")
 
 @app.post("/predict/")
 async def predict(request:Request, file: UploadFile = File(...)):
+
+    num_requests.labels(method="POST", endpoint="/predict", ip_address=request.client.host).inc()
+
     start_time = time.time()
     image = await file.read()
     
     # Get client IP address
-    client_ip = request.client.host
+    # client_ip = request.client.host
     
     # Increment API usage counter
-    api_usage_counter.labels(client_ip).inc()
+    # api_usage_counter.labels(client_ip).inc()
 
    
     pil_image = PIL.Image.open(io.BytesIO(image))
@@ -85,17 +93,24 @@ async def predict(request:Request, file: UploadFile = File(...)):
     total_time = end_time - start_time
     
     # Set API processing time gauges
-    api_time_gauge.labels(client_ip).set(total_time)
-    api_time_per_char_gauge.labels(client_ip).set(total_time / len(file.filename))
+    # api_time_gauge.labels(client_ip).set(total_time)
+    # api_time_per_char_gauge.labels(client_ip).set(total_time / len(file.filename))
     
     image = Image.fromarray((masked_img * 255).astype(np.uint8))
 
     # Create a BytesIO object and save the PIL image to it
     img_io = io.BytesIO()
     image.save(img_io, 'PNG', quality=70)
-
-    # Seek to the beginning of the BytesIO object
+    
     img_io.seek(0)
+
+    end_time = time.time()    
+    # System Metrics Update
+    memory_usage.set(psutil.virtual_memory().used)
+    cpu_usage.set(psutil.cpu_percent())
+    net_io = psutil.net_io_counters()
+    network_io_received.inc(net_io.bytes_recv)
+    network_io_sent.inc(net_io.bytes_sent)
 
     # Return a StreamingResponse to send the image
     return StreamingResponse(img_io, media_type='image/png')
